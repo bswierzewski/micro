8793fc956be1e737ce1ec93e21b0af7a19450feb
b.swierzewski <swierzewski.bartosz@gmail.com>
2021-05-01 17:14:59 +0200

    Create seed storeContext method and use them in Program.cs

---
+++ services/Infrastructure/Data/Extensions/StoreContextSeedExtensions.cs
@@ -0,0 +1,31 @@
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data.Extensions
+{
+    public static class StoreContextSeedExtensions
+    {
+        private const string path = "../../Mocks";
+        public static async Task DeserializeJson<T>(this StoreContext context, string fileName) where T : class
+        {
+            var set = context.Set<T>();
+
+            if (!set.Any())
+            {
+                var json = File.ReadAllText(Path.Combine(path, $"{fileName}.json"));
+
+                var items = JsonSerializer.Deserialize<T[]>(json, new JsonSerializerOptions
+                {
+                    WriteIndented = true,
+                    PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+                });
+
+                await set.AddRangeAsync(items);
+
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+}
---
+++ services/Infrastructure/Data/StoreContextSeed.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Threading.Tasks;
+using Core.Entities;
+using Infrastructure.Data.Extensions;
+using Microsoft.Extensions.Logging;
+using Version = Core.Entities.Version;
+
+namespace Infrastructure.Data
+{
+    public class StoreContextSeed
+    {
+        public static async Task SeedAsync(StoreContext context, ILoggerFactory logger)
+        {
+            try
+            {
+                await context.DeserializeJson<Kind>("kinds");
+                await context.DeserializeJson<Category>("categories");
+                await context.DeserializeJson<Address>("addresses");
+                await context.DeserializeJson<Component>("components");
+                await context.DeserializeJson<VersionData>("versionsData");
+                await context.DeserializeJson<Version>("versions");
+                await context.DeserializeJson<Device>("devices");
+            }
+            catch (Exception ex)
+            {
+                logger.CreateLogger<StoreContextSeed>().LogError(ex.Message);
+            }
+        }
+    }
+}
