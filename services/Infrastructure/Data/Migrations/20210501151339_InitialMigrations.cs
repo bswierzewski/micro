13483d17c9e6c9ecc104ca8a6c8d8a199716f3d6
b.swierzewski <swierzewski.bartosz@gmail.com>
2021-05-01 17:14:34 +0200

    Add initial migrations

---
+++ services/Infrastructure/Data/Migrations/20210501151339_InitialMigrations.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Data.Migrations
+{
+    public partial class InitialMigrations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Addresses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Label = table.Column<string>(type: "TEXT", nullable: true),
+                    AddressType = table.Column<int>(type: "INTEGER", nullable: false),
+                    IsConfirmed = table.Column<bool>(type: "INTEGER", nullable: false),
+                    Created = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    CreatedBy = table.Column<int>(type: "INTEGER", nullable: false),
+                    Modified = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    ModifiedBy = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Addresses", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    Icon = table.Column<string>(type: "TEXT", nullable: true),
+                    Created = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    CreatedBy = table.Column<int>(type: "INTEGER", nullable: false),
+                    Modified = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    ModifiedBy = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Kinds",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    Icon = table.Column<string>(type: "TEXT", nullable: true),
+                    Created = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    CreatedBy = table.Column<int>(type: "INTEGER", nullable: false),
+                    Modified = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    ModifiedBy = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Kinds", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "VersionsData",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    Extension = table.Column<string>(type: "TEXT", nullable: true),
+                    Content = table.Column<byte[]>(type: "BLOB", nullable: true),
+                    Created = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    CreatedBy = table.Column<int>(type: "INTEGER", nullable: false),
+                    Modified = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    ModifiedBy = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VersionsData", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Components",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    Icon = table.Column<string>(type: "TEXT", nullable: true),
+                    CategoryId = table.Column<int>(type: "INTEGER", nullable: true),
+                    Created = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    CreatedBy = table.Column<int>(type: "INTEGER", nullable: false),
+                    Modified = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    ModifiedBy = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Components", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Components_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Versions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    Major = table.Column<short>(type: "INTEGER", nullable: false),
+                    Minor = table.Column<short>(type: "INTEGER", nullable: false),
+                    Patch = table.Column<short>(type: "INTEGER", nullable: false),
+                    ComponentId = table.Column<int>(type: "INTEGER", nullable: true),
+                    KindId = table.Column<int>(type: "INTEGER", nullable: true),
+                    VersionDataId = table.Column<int>(type: "INTEGER", nullable: true),
+                    Created = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    CreatedBy = table.Column<int>(type: "INTEGER", nullable: false),
+                    Modified = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    ModifiedBy = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Versions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Versions_Components_ComponentId",
+                        column: x => x.ComponentId,
+                        principalTable: "Components",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Versions_Kinds_KindId",
+                        column: x => x.KindId,
+                        principalTable: "Kinds",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Versions_VersionsData_VersionDataId",
+                        column: x => x.VersionDataId,
+                        principalTable: "VersionsData",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Devices",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    Icon = table.Column<string>(type: "TEXT", nullable: true),
+                    AddressId = table.Column<int>(type: "INTEGER", nullable: true),
+                    KindId = table.Column<int>(type: "INTEGER", nullable: true),
+                    ComponentId = table.Column<int>(type: "INTEGER", nullable: true),
+                    VersionId = table.Column<int>(type: "INTEGER", nullable: true),
+                    IsUpdated = table.Column<bool>(type: "INTEGER", nullable: true),
+                    Updated = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    IsAutoUpdate = table.Column<bool>(type: "INTEGER", nullable: true),
+                    Created = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    CreatedBy = table.Column<int>(type: "INTEGER", nullable: false),
+                    Modified = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    ModifiedBy = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Devices", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Devices_Addresses_AddressId",
+                        column: x => x.AddressId,
+                        principalTable: "Addresses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Devices_Components_ComponentId",
+                        column: x => x.ComponentId,
+                        principalTable: "Components",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Devices_Kinds_KindId",
+                        column: x => x.KindId,
+                        principalTable: "Kinds",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Devices_Versions_VersionId",
+                        column: x => x.VersionId,
+                        principalTable: "Versions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Components_CategoryId",
+                table: "Components",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Devices_AddressId",
+                table: "Devices",
+                column: "AddressId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Devices_ComponentId",
+                table: "Devices",
+                column: "ComponentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Devices_KindId",
+                table: "Devices",
+                column: "KindId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Devices_VersionId",
+                table: "Devices",
+                column: "VersionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Versions_ComponentId",
+                table: "Versions",
+                column: "ComponentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Versions_KindId",
+                table: "Versions",
+                column: "KindId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Versions_VersionDataId",
+                table: "Versions",
+                column: "VersionDataId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Devices");
+
+            migrationBuilder.DropTable(
+                name: "Addresses");
+
+            migrationBuilder.DropTable(
+                name: "Versions");
+
+            migrationBuilder.DropTable(
+                name: "Components");
+
+            migrationBuilder.DropTable(
+                name: "Kinds");
+
+            migrationBuilder.DropTable(
+                name: "VersionsData");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
