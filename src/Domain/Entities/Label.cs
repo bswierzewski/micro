85c8bf7856aa04a5f394a39e605ec14afda01712
bswierzewski <swierzewski.bartosz@gmail.com>
2021-06-15 13:59:41 +0200

    Add label event

--- src/Domain/Entities/Label.cs
+++ src/Domain/Entities/Label.cs
@@ -1,13 +1,16 @@
 ﻿using micro_api.Domain.Common;
 using micro_api.Domain.Enums;
+using System.Collections.Generic;
 
 namespace micro_api.Domain.Entities
 {
-    public class Label : AuditableEntity
+    public class Label : AuditableEntity, IHasDomainEvent
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public AddressType AddressType { get; set; } = AddressType.Mac;
         public bool IsConfirmed { get; set; }
+
+        public List<DomainEvent> DomainEvents { get; set; } = new List<DomainEvent>();
     }
 }
---
+++ src/Domain/Events/Label/LabelCreatedEvent.cs
@@ -0,0 +1,14 @@
+﻿using micro_api.Domain.Common;
+
+namespace micro_api.Domain.Events.Label
+{
+    class LabelCreatedEvent : DomainEvent
+    {
+        public Entities.Label Label { get; }
+
+        public LabelCreatedEvent(Entities.Label label)
+        {
+            Label = label;
+        }
+    }
+}
