6d6f20cf81ba9b1e761b7e597e257abb7000804c
bswierzewski <swierzewski.bartosz@gmail.com>
2021-06-16 13:23:56 +0200

    Add unique index to label entity in database

---
+++ src/Infrastructure/Persistence/Configurations/LabelConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using micro_api.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace micro_api.Infrastructure.Persistence.Configurations
+{
+    public class LabelConfiguration : IEntityTypeConfiguration<Label>
+    {
+        public void Configure(EntityTypeBuilder<Label> builder)
+        {
+            builder.HasIndex(l => l.Name)
+                   .IsUnique();
+
+            builder.Property(l => l.Name)
+                   .IsRequired();
+        }
+    }
+}
---
+++ src/Infrastructure/Persistence/Migrations/20210616111521_Label unique index.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace micro_api.Infrastructure.Persistence.Migrations
+{
+    public partial class Labeluniqueindex : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Labels",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Labels_Name",
+                table: "Labels",
+                column: "Name",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Labels_Name",
+                table: "Labels");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Labels",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+        }
+    }
+}
