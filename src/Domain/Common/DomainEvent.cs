c6276f85db8b33a9c5512e2d503b5af98f7b5b9a
bswierzewski <swierzewski.bartosz@gmail.com>
2021-06-16 14:42:23 +0200

    Clean up DomainEvent class

--- src/Domain/Common/DomainEvent.cs
+++ src/Domain/Common/DomainEvent.cs
@@ -10,10 +10,6 @@ namespace micro_api.Domain.Common
 
     public abstract class DomainEvent
     {
-        protected DomainEvent()
-        {
-            DateOccurred = DateTimeOffset.UtcNow;
-        }
         public bool IsPublished { get; set; }
         public DateTimeOffset DateOccurred { get; protected set; } = DateTime.UtcNow;
     }
